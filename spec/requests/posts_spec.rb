544a20e7246e3d0f9230432c540777973ff70d05
Thamindu Liyanage <thaminduliyanage97@gmail.com>
2025-06-23 00:06:28 +0530

    TDD model and controller

---
+++ spec/requests/posts_spec.rb
@@ -0,0 +1,41 @@
+require 'rails_helper'
+
+RSpec.describe "Posts", type: :request do
+  describe "/posts/new" do
+    it "sucessfully renders the new post form" do
+      get new_post_path
+      expect(response).to  have_http_status(:success)
+    end
+  end
+  describe "/posts/create" do
+
+    def create_post(title,body)
+      post posts_path, params: {
+        post: {
+          title: title,
+          body: body
+        }
+      }
+    end
+
+    context "valid params" do
+      it "sucessfully create a post" do
+        expect do 
+          create_post("Test Post", "This is a test post body")
+        end.to change {Post.count}.from(0).to(1)
+        expect(response).to have_http_status(:redirect)  
+      end
+    end
+
+    context "invalid params" do
+      it "failed at creating post" do
+        expect do
+          create_post("", "")
+        end.to_not change {Post.count}
+        expect(Post.count).to eq(0)
+        expect(response).to have_http_status(:success)
+      end
+    end
+    
+  end
+end
